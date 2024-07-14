4c4535637bfc1439b155571f19b0a87577d7b69f
Mohnish <mohnish.a007@gmail.com>
2024-07-14 20:52:37 +0100

    dummy api attached for countries: make sure the brackets are correct while fetching data in react

--- src/conponents/Mapsimple.jsx
+++ src/conponents/Mapsimple.jsx
@@ -1,10 +1,24 @@
-import React from "react"
+import React,{useEffect,useState,useRef} from "react"
 import { ComposableMap, Geographies, Geography } from "react-simple-maps"
 //import {world} from "../world.json"
 const geoUrl = "https://raw.githubusercontent.com/lotusms/world-map-data/main/world.json"
 
 export default function Mapsimple() {
+    const [countries, setCountries] = useState([]);
+    const getData = ()=>{
+        fetch('http://localhost:3004/countries', {
+            headers: {'Content-Type':'application/json', 'Accept':'application/json'},
+        })
+        .then((response)=>response.json())
+        .then((data) => setCountries(data))
+    }
+    useEffect(()=>{
+        getData()
+    },[])
+   // console.log(countries)
   return (
+    <>
+   
     <ComposableMap>
       <Geographies geography={geoUrl}  >
         {({ geographies }) =>
@@ -24,5 +38,18 @@ export default function Mapsimple() {
         }
       </Geographies>
     </ComposableMap>
+    <div>
+        <h1>Country List</h1>
+        
+        {countries.map((country,index)=>{
+            return (
+                <div key={index}>{country.name}</div>
+            )
+        })}
+        {/* {countries.map((country, index)=>(
+            <div key={index}>{country.name}</div>
+        ))} */}
+    </div>
+    </>
   )
 }
