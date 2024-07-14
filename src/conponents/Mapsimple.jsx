70df483e1dd028b28265cbfa31fd558e8713e229
Mohnish <mohnish.a007@gmail.com>
2024-07-14 01:41:55 +0100

    new world with geoURL

--- src/conponents/Mapsimple.jsx
+++ src/conponents/Mapsimple.jsx
@@ -1,13 +1,12 @@
 import React from "react"
 import { ComposableMap, Geographies, Geography } from "react-simple-maps"
-import {world} from "../world.json"
-const geoUrl = world
-  //"https://raw.githubusercontent.com/lotusms/world-map-data/main/world.json"
+//import {world} from "../world.json"
+const geoUrl = "https://raw.githubusercontent.com/lotusms/world-map-data/main/world.json"
 
 export default function Mapsimple() {
   return (
     <ComposableMap>
-      <Geographies geography={world}>
+      <Geographies geography={geoUrl}>
         {({ geographies }) =>
           geographies.map((geo) => (
             <Geography key={geo.rsmKey} geography={geo} />
